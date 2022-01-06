07518f8d46ad785ce34cfcaf5eecd1fc066cd247
tmhuyett <toddhuyett@gmail.com>
2022-01-05 22:19:34 -0500

    'd'

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
 var cors = require("cors")
+const sql = require('mssql')
 const PORT = process.env.PORT || 3000;
 
+const config = {
+  user: 'admin',
+  password: process.env.NAME,
+  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+  database: process.env.PWD,
+}
+
 app.get('/', (req, res) => {
+  async () => {
+    try {
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect(config)
+        const result = await sql.query`select * from Persons`
+        res.send('Great shit!')
+    } catch (err) {
+        // ... error checks
+    }
+}
   res.send('Hello World!')
 })
 
