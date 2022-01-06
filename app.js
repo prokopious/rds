20a7da03d9b3a91f7ce36fcff2e2609ce0e1b871
tmhuyett <toddhuyett@gmail.com>
2022-01-05 20:15:58 -0500

    'd'

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express")
+var app = express()
+const sql = require('mssql')
+
+
+
+app.get("/", function (req, res) {
+
+
+  // config for your database
+  var config = {
+    user: "admin",
+    password: "Khazarkhan1!",
+    server: "localhost",
+    database:
+      "database-1.cz8tqqetywxp.us-east-1.rds.amazonaws.com",
+  }
+
+  // connect to your database
+  sql.connect(config, function (err) {
+    if (err) console.log(err)
+
+    // create Request object
+    var request = new sql.Request()
+
+    // query to the database and get the records
+    request.query("select * from Persons", function (err, recordset) {
+      if (err) console.log(err)
+
+      // send records as a response
+      res.send(recordset)
+    })
+  })
+})
+
+var server = app.listen(5000, function () {
+  console.log("Server is running..")
+})
