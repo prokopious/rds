7ffe83dfddd4b1fb1bcd41a12e2c30ee3cd55034
tmhuyett <toddhuyett@gmail.com>
2022-01-05 21:18:22 -0500

    'd'

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-var express = require("express")
-var app = express()
-const sql = require('mssql')
+const express = require('express')
+const app = express()
+const port = 3000
 
-
-
-app.get("/", function (req, res) {
-
-
-  // config for your database
-  var config = {
-    user: "admin",
-    password: "Khazarkhan1!",
-    server: "localhost",
-    database:
-      "database-1.cz8tqqetywxp.us-east-1.rds.amazonaws.com",
-  }
-
-  // connect to your database
-  sql.connect(config, function (err) {
-    if (err) console.log(err)
-
-    // create Request object
-    var request = new sql.Request()
-
-    // query to the database and get the records
-    request.query("select * from Persons", function (err, recordset) {
-      if (err) res.send(err)
-
-      // send records as a response
-      res.send("shit")
-    })
-  })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
