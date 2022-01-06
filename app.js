9317aaaf1b4ca8263530134b311e34ba43d27efd
tmhuyett <toddhuyett@gmail.com>
2022-01-06 02:30:10 -0500

    'd'

--- app.js
+++ app.js
@@ -5,42 +5,33 @@ require("dotenv").config()
 
 const PORT = process.env.PORT || 1433
 app.use(cors())
-app.get("/", async (req, res) => {
-  var sql = require("mssql");
+app.get("/", (req, res) => {
+  var sql = require("mssql")
   const config = {
     user: "admin",
     password: process.env.PWD_D,
     server: process.env.NAME_D,
-    synchronize: true,
-   extra: { 
-    trustServerCertificate: false,
-    Encrypt: true,
-    IntegratedSecurity: true,
-    
-    
-    },
-    database: "liberty"
+    database: 'liberty',
   }
 
   sql.connect(config, function (err) {
-    
-    if (err) console.log(err);
+    if (err) console.log(err)
 
     // create Request object
-    var request = new sql.Request();
-       
+    var request = new sql.Request()
+
     // query to the database and get the records
     request.query('select * from Persons', function (err, recordset) {
-        
-        if (err) console.log(err)
-
-        // send records as a response
-        res.send(JSON.stringify(recordset));
-        
-    });
-});
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(recor)
+        res.send("shit")
+      }
+    })
+  })
 })
 
 app.listen(PORT, () => {
-  console.log(`Our app is running on port ${ PORT }`);
-});
+  console.log(`Our app is running on port ${PORT}`)
+})
