99956c96ff3be0c312aff92637f2327d1226bb31
tmhuyett <toddhuyett@gmail.com>
2022-01-06 02:04:29 -0500

    'd'

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/", async (req, res) => {
         if (err) console.log(err)
 
         // send records as a response
-        res.send(recordset);
+        res.send("shit");
         
     });
 });
