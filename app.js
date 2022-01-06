5cae45b8f0fdb43fec8d853f320e048e713b4cfa
tmhuyett <toddhuyett@gmail.com>
2022-01-06 00:18:50 -0500

    'd'

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 var cors = require("cors")
-const sql = require('mssql')
-const PORT = process.env.PORT || 3000;
+const sql = require("mssql")
+require("dotenv").config()
+const PORT = process.env.PORT
 
-const config = {
-  user: 'admin',
-  password: process.env.NAME,
-  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
-  database: process.env.PWD,
-}
+app.get("/", async (req, res) => {
+  const config = {
+    user: "admin",
+    password: process.env.PWD_D,
+    server: process.env.NAME_D, 
+    database: "liberty",
+  }
 
-app.get('/', (req, res) => {
-  async () => {
-    try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect(config)
-        const result = await sql.query`select * from Persons`
-        res.send('Great shit!')
-    } catch (err) {
-        // ... error checks
-    }
-}
-  res.send('Hello World!')
+  try {
+    await sql.connect(config)
+    const result = await sql.query`select * from Persons`
+    console.log("shit")
+    res.send("Great shit!")
+  } catch (err) {
+    console.log("this sucks")
+  }
 })
 
 app.listen(PORT, () => {
-  console.log(`Our app is running on port ${ PORT }`);
-});
+  console.log(`Our app is running on port ${PORT}`)
+})
