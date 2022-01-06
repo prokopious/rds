12555905eb9223f1182668869c669c9563179426
tmhuyett <toddhuyett@gmail.com>
2022-01-06 00:53:15 -0500

    'd'

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.get("/", async (req, res) => {
     database: "liberty",
     port: process.env.PORT
   }
-
-  try {
-    await sql.connect(config)
-    const result = await sql.query`select * from Persons`
-    console.log("shit")
-    res.send("Great!")
-  } catch (err) {
-    console.log("this sucks")
-  }
+res.send(JSON.stringify(config))
+  // try {
+  //   await sql.connect(config)
+  //   const result = await sql.query`select * from Persons`
+  //   console.log("shit")
+  //   res.send("Great!")
+  // } catch (err) {
+  //   console.log("this sucks")
+  // }
 })
 
 app.listen(PORT, () => {
